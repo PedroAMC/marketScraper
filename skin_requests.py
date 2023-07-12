a629b7a719a2f46ed2f393ed7143cddfbc1f5a9b
Pedro António Morgado Campos <77537974+PedroAMC@users.noreply.github.com>
2023-07-12 22:56:20 +0100

    Initial commit

---
+++ skin_requests.py
@@ -0,0 +1,180 @@
+import time
+from bs4 import BeautifulSoup
+import requests
+import base64
+from requests_html import HTMLSession
+from selenium import webdriver
+import undetected_chromedriver as uc
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+chrome_options.add_argument("start-maximized");
+chrome_options.add_argument("disable-infobars");
+chrome_options.add_argument("--disable-extensions");
+chrome_options.add_argument("--disable-gpu");
+chrome_options.add_argument("--disable-dev-shm-usage");
+chrome_options.add_argument("--user-data-dir=C:\\Users\\pcped\\AppData\\Local\\Google\\Chrome\\User Data")
+chrome_options.add_argument('--profile-directory=Default')
+
+
+driver = webdriver.Chrome(options=chrome_options)
+
+
+weapons = {
+    "AK-47": "weapon_ak47",
+    "AWP": "weapon_awp",
+    "M4A4": "weapon_m4a1",
+    "M4A1-S": "weapon_m4a1_silencer",
+    "USP-S": "weapon_usp_silencer",
+    "Glock-18": "weapon_glock",
+    "Desert Eagle": "weapon_deagle",
+    "Knife": "knife",
+    "Gloves": "hands"
+}
+
+conditions = {
+    "Factory New": "wearcategory0",
+    "Minimal Wear": "wearcategory1",
+    "Field-Tested": "wearcategory2",
+    "Well-Worn": "wearcategory3",
+    "Battle-Scarred": "wearcategory4"
+}
+
+class Skin:
+    def __init__(self, name, buffPrice, condition):
+        self.name = name
+        self.buffPrice = buffPrice
+        self.condition = condition
+        self.skinPortPrice = 0
+        self.profit = 0
+        self.profitPercentage = 0
+        self.iconUrl = "";
+
+
+
+
+
+
+def get_skins(weapon_name, condition, min_price, max_price):
+    print("running scraper")
+    weapon_name = weapons[weapon_name]
+    condition = conditions[condition]
+    if (weapon_name != "knife" or weapon_name != "hands"):
+        driver.get(
+            f"https://buff.163.com/market/csgo#tab=selling&page_num=1&category={weapon_name}&min_price={min_price}&max_price={max_price}&exterior={condition}")
+    else:
+        driver.get(
+            f"https://buff.163.com/market/csgo#tab=selling&page_num=1&category_group={weapon_name}&min_price={min_price}&max_price={max_price}&exterior={condition}")
+
+    user_agent = driver.execute_script("return navigator.userAgent;")
+    print(user_agent)
+    #div_element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "j_list_card")))
+    print(driver.title)
+    print(driver.current_url)
+
+    print(user_agent)
+
+    #weapon_cards_list = div_element.find_element(By.TAG_NAME, "ul")
+    #weapon_cards = weapon_cards_list.find_elements_by_css_selector("li")
+
+    #for weapon_card in weapon_cards:
+       #print(weapon_card.text)
+
+    #driver.close()
+
+def get_skins_bs4(weapon_name, condition, min_price, max_price):
+    querySkins = []
+    weapon_name = weapons[weapon_name]
+    condition = conditions[condition]
+
+    print("running bs4")
+
+
+    cookies = {
+        'Device-Id': 'G4fFCRE0U92s5dsUF5q8',
+        'P_INFO': '351-911556587|1685714431|1|netease_buff|00&99|null&null&null#PT&null#10#0|&0||351-911556587',
+        'Locale-Supported': 'en',
+        'game': 'csgo',
+        'qr_code_verify_ticket': 'be5CAuY0913e9edc4805ae380dd8c47af988',
+        'remember_me': 'U1103613490|IvTgODLkmEeZwFChbCzshRUTf8wTU6Qp',
+        'session': '1-kzDt6A7o9YjXYQk3MWcCIoZkc_jAOgjZZf8fJUnnYDdN2030653802',
+        'csrf_token': 'IjlhOWQxZTQ0M2IzNmUzYWMzNzU0YWFiMTEwOGIzMTQ3ZjIxNTE2ZjEi.F4-sww.1Jon4R57YrBQO-dQvndDx70lNtc',
+    }
+
+    headers = {
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Connection': 'keep-alive',
+        # 'Cookie': 'Device-Id=G4fFCRE0U92s5dsUF5q8; P_INFO=351-911556587|1685714431|1|netease_buff|00&99|null&null&null#PT&null#10#0|&0||351-911556587; Locale-Supported=en; game=csgo; qr_code_verify_ticket=be5CAuY0913e9edc4805ae380dd8c47af988; remember_me=U1103613490|IvTgODLkmEeZwFChbCzshRUTf8wTU6Qp; session=1-kzDt6A7o9YjXYQk3MWcCIoZkc_jAOgjZZf8fJUnnYDdN2030653802; csrf_token=IjlhOWQxZTQ0M2IzNmUzYWMzNzU0YWFiMTEwOGIzMTQ3ZjIxNTE2ZjEi.F4-sww.1Jon4R57YrBQO-dQvndDx70lNtc',
+        'Referer': 'https://buff.163.com/market/csgo',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36',
+        'X-Requested-With': 'XMLHttpRequest',
+        'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
+        'sec-ch-ua-mobile': '?1',
+        'sec-ch-ua-platform': '"Android"',
+    }
+
+    params = {
+        'game': 'csgo',
+        'page_num': '1',
+        'category': weapon_name,
+        'min_price': min_price,
+        'max_price': max_price,
+        'exterior': condition,
+        'use_suggestion': '0',
+        '_': '1689131300601',
+    }
+
+    clientId = "063ed991124a407bb6809f1bb6b637ea"
+    clientSecret = "tRyVL1b3v86RJpxctrl+zkx13JXIL6fu6C5Z+sUiwg8K+baIHiWcEeemqUN2Ug2W01Km968Dw5VhGKm0let4ew=="
+    clientData = f"{clientId}:{clientSecret}"
+    encodedData = str(base64.b64encode(clientData.encode("utf-8")), "utf-8")
+    authorizationHeaderString = f"Basic {encodedData}"
+
+    response = requests.get('https://buff.163.com/api/market/goods', params=params, cookies=cookies, headers=headers)
+
+    queryString = ""
+    #print(response.json())
+    for item in response.json()["data"]["items"]:
+        short_name = item["short_name"]
+        buff_price = float(item["sell_min_price"])*0.12610534
+        condition = item["goods_info"]["info"]["tags"]["exterior"]["localized_name"]
+        icon_url = item["goods_info"]["icon_url"]
+        queryString += f"{short_name} ({condition}),"
+        skin = Skin(short_name, buff_price, condition)
+        skin.icon_url = icon_url
+
+        querySkins.append(skin)
+
+
+    queryString = queryString[:-1]
+    #print(queryString)
+
+    r = requests.get("https://api.skinport.com/v1/sales/history", headers={
+    "authorization": authorizationHeaderString}, params={
+            #"market_hash_name": queryString,
+            "app_id": 730,
+            "currency": "EUR"
+
+        }).json()
+
+    for item in r:
+        for skin in querySkins:
+            skin_name = skin.name + " (" + skin.condition + ")"
+            if skin_name == item["market_hash_name"]:
+                #print(item)
+                month_average = item["last_30_days"]["avg"]
+                skin.skinPortPrice = float(month_average)
+                skin.profit = skin.skinPortPrice*0.88 - skin.buffPrice
+                skin.profitPercentage = skin.profit/skin.buffPrice*100
+                #print(f"Name: {skin.name} Condition: {skin.condition} Buff Price: {skin.buffPrice} Skinport Price: {month_average} Profit: {skin.profit}€ Percentage: {skin.profitPercentage}%")
+    return querySkins
+
+
---
+++ views.py
@@ -0,0 +1,52 @@
+from flask import Blueprint, render_template, request, jsonify, redirect, url_for
+from skin_requests import get_skins, get_skins_bs4
+from threading import Timer
+
+
+
+views = Blueprint(__name__, "views")
+
+PASSWORD = "aiaiomano"
+authenticated = False
+
+@views.route("/")
+def home():
+    global authenticated
+    if not authenticated:
+        return redirect(url_for("views.login"))
+    weapons = ["AK-47", "AWP", "M4A4", "M4A1-S", "USP-S", "Glock-18", "Desert Eagle", "Knife", "Gloves"]
+    conditions = ["Factory New", "Minimal Wear", "Field-Tested", "Well-Worn", "Battle-Scarred"]
+
+    return render_template("index.html", weapons=weapons, conditions=conditions)
+
+
+@views.route("/login", methods=["GET", "POST"])
+def login():
+    global authenticated
+    if authenticated:
+        return redirect(url_for("views.home"))
+
+    if request.method == "POST":
+        password = request.form.get("password")
+        if password == PASSWORD:
+            authenticated = True
+            return redirect(url_for("views.home"))
+        else:
+            return render_template("login.html", error="Invalid password")
+
+    return render_template("login.html", error=None)
+
+@views.route("/weapon", methods=["GET"])
+def weapon():
+    weapon_name = request.args.get("weapon")
+    condition = request.args.get("condition")
+    min_price = request.args.get("min_price")
+    max_price = request.args.get("max_price")
+    skins = get_skins_bs4(weapon_name, condition, min_price, max_price)
+    sortedSkins = sorted(skins, key=lambda x: x.profitPercentage, reverse=True)
+    sortedSkins = [skin for skin in sortedSkins if skin.profitPercentage <= 55]
+    sortedSkins = sortedSkins[:10]
+
+    return render_template("weapon.html", weapon_name=weapon_name, condition=condition, min_price=min_price, max_price=max_price, skins=sortedSkins)
+
+
